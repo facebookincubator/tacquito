35d6054b04cbc7687ae725fb2c38797584337bf2
generatedunixname896326506490493 <noreply+896326506490493@fb.com>
2026-07-21 00:35:10 -0700

    Fix modernize issues in fbcode/nise/nsa/tacquito/github.com/facebookincubator/tacquito/cmds/server/config/secret/prefix
    Differential Revision: D112774621
    
    fbshipit-source-id: 85209e43dc19e4011f022a1466b7938fe56ad016

--- cmds/server/config/secret/prefix/provider.go
+++ cmds/server/config/secret/prefix/provider.go
@@ -19,9 +19,9 @@ import (
 
 // loggerProvider provides the logging implementation
 type loggerProvider interface {
-	Infof(ctx context.Context, format string, args ...interface{})
-	Errorf(ctx context.Context, format string, args ...interface{})
-	Debugf(ctx context.Context, format string, args ...interface{})
+	Infof(ctx context.Context, format string, args ...any)
+	Errorf(ctx context.Context, format string, args ...any)
+	Debugf(ctx context.Context, format string, args ...any)
 	Record(ctx context.Context, r map[string]string, obscure ...string)
 }
 
